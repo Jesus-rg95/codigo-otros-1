6d0f1b82faaad036ab80611fa1e4bb4927bbe4e9
jesusgr <jesusgr9511@gmail.com>
2025-05-22 15:30:10 -0600

    Se suben arreglos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//le falto . a la clase
+const $b = document.querySelector('.blog');//# esta mal es un .
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//lefalta la palabra async a la funcion
+ async function displayUser(username) { 
   $n.textContent = 'cargando...';
+try{
   const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); //obtener los datos
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;//Se LE AGREGAN BACTIS
+}catch (err) {
+   handleError(err);
+}
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`// Le falta el $ para que este bien definido
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
